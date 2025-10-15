ab1dda4f01f45bf97dc8acbdc080a192ee24db55
Ylarod <me@ylarod.cn>
2025-10-15 11:47:25 +0800

    ddk: support local mode

--- module_template/envsetup.sh
+++ module_template/envsetup.sh
@@ -1,12 +1,12 @@
 export DDK_ROOT=/opt/ddk
 
-export KERNEL_SRC=$DDK_ROOT/kdir/android12-5.10
+export KDIR=$DDK_ROOT/kdir/android12-5.10
 export CLANG_PATH=$DDK_ROOT/clang/clang-r416183b/bin
 
-# export KERNEL_SRC=$DDK_ROOT/kdir/android13-5.15
+# export KDIR=$DDK_ROOT/kdir/android13-5.15
 # export CLANG_PATH=$DDK_ROOT/clang/clang-r450784e/bin
 
-# export KERNEL_SRC=$DDK_ROOT/kdir/android14-6.1
+# export KDIR=$DDK_ROOT/kdir/android14-6.1
 # export CLANG_PATH=$DDK_ROOT/clang/clang-r487747c/bin
 
 # export CLANG_PATH=/home/ylarod/Android/Sdk/ndk/25.2.9519653/toolchains/llvm/prebuilt/linux-x86_64/bin
